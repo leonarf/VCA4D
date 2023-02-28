6056cd6cacf68d95c326cd8a03df5727894cf2a5
David Bruant <davidbruant@protonmail.com>
2023-02-28 22:17:44 +0100

    Squelette (#2)
    * Un peu un squelette
    
    * Début de page d'accueil

---
+++ src/components/Skeleton.vue
@@ -0,0 +1,61 @@
+<script>
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+    <header>
+        <RouterLink to="/">VCA4D website</RouterLink>
+
+        <div>
+            <img src="/images/logo/agrinatura.jpeg" alt="agrinatura network logo">
+            <img src="/images/logo/VCA4D.jpeg" alt="VCA4D logo">
+            <img src="/images/logo/european-commission.jpeg" alt="european commission logo">
+        </div>
+    </header>
+
+    <main>
+        <slot></slot>
+    </main>
+
+    <footer>
+        FOOTER
+    </footer>
+</template>
+
+<style scoped lang="scss">
+header{
+    height: 10rem;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & > a{
+        font-size: 2rem;
+        font-weight: bold;
+        color: #929292;
+        text-transform: uppercase;
+        text-decoration: none;
+    }
+
+    & > div{
+        height: 100%;
+
+        display: flex;
+        flex-direction: row;
+
+        img{
+            height: 100%;
+            width: 7rem;
+            margin: 0 1rem;
+            background-color: grey;
+
+            text-align: center;
+        }
+    }
+
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,92 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Skeleton from '../components/Skeleton.vue'
 </script>
 
 <template>
-  <main>
-    <h1>Get insight into food value chains in developing countries</h1>
-    <p>
-      Welcome to the VCA4D website. You will find a tailored access to 35 value chain analyses
-      across 16 different agricultural commodities around the world. Based on a standardised
-      methodology (*link), each analysis allows an assessment of the value chain's impact on
-      smallholders, businesses, society and environment. Any of the VCA4D academic study provides
-      information across 4 related domains:
-    </p>
-    <ul>
-      <li>the various actors and structuration of the chain</li>
-      <li>its contribution to economic growth and the state's public finance</li>
-      <li>the chain's inclusiveness and contribution to employment</li>
-      <li>the impact of the value chain on environment.</li>
-    </ul>
-    <TheWelcome />
-  </main>
+  <Skeleton>
+    <section class="banner">
+      <div>
+        <h1>Value Chain Analysis For Development</h1>
+        <h2>Providing value chain analysis for improving operations</h2>
+      </div>
+    </section>
+
+    <section>
+      <h2>Get insight into food value chains in developing countries</h2>
+      <p>
+        Welcome to the VCA4D website. You will find a tailored access to 35 value chain analyses
+        across 16 different agricultural commodities around the world. Based on a standardised
+        methodology (*link), each analysis allows an assessment of the value chain's impact on
+        smallholders, businesses, society and environment. Any of the VCA4D academic study provides
+        information across 4 related domains:
+      </p>
+      <ul>
+        <li>the various actors and <strong>structuration of the chain</strong></li>
+        <li>its contribution to <strong>economic growth</strong> and the state's public finance</li>
+        <li>the chain's <strong>inclusiveness</strong> and contribution to <strong>employment</strong></li>
+        <li>the impact of the value chain on <strong>environment</strong>.</li>
+      </ul>
+    </section>
+  </Skeleton>
 </template>
+
+<style scoped lang="scss">
+
+:global(main){
+  padding: 0 !important;
+}
+
+section{
+  padding: 1.5rem;
+}
+
+section.banner{
+  height: 25rem;
+
+  background-image: url('../images/home-banner.png');
+  background-position: center center;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+
+  div{  
+    height: 60%;
+    max-width: 50%;
+    padding: 1rem;
+    border-radius: 2rem;
+    background-color: hsla(0,0,0, 0.7);
+
+    position: relative;
+    left: 4rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: flex-start;
+
+    h1, h2{
+      color: white;
+      margin: 0;
+    }
+
+    h1{
+      font-size: 3rem;
+      line-height: 1.2;
+    }
+
+    h2{
+      font-size: 1.2rem;
+    }
+  }
+  
+
+}
+
+
+ul{
+  list-style-type: "- ";
+}
+
+</style>
