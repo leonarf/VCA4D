e6e7258ab961803028d84f1af91f8b1af1e48aee
leonard <leonard@lebasic.com>
2023-06-05 15:00:20 +0200

    ajout de couleur au graph bar du retour sur investissement

--- src/components/StudyEconomicGrowth.vue
+++ src/components/StudyEconomicGrowth.vue
@@ -1,135 +1,158 @@
 <template>
-    <article class="study-section">
-        <h1>What is the contribution of the value chain to <strong>economic growth</strong>?</h1>
+  <article class="study-section">
+    <h1>What is the contribution of the value chain to <strong>economic growth</strong>?</h1>
 
-        <p>VCA4D analyses shows not only the value chain's contribution to the national wealth and to the wealth in agriculture. The methodology has also developped data on the distribution of the net benefits across the various actors of the value chain, includting the contribution left to the state budget, after public investment in the value chain.</p>
+    <p>
+      VCA4D analyses shows not only the value chain's contribution to the national wealth and to the
+      wealth in agriculture. The methodology has also developped data on the distribution of the net
+      benefits across the various actors of the value chain, includting the contribution left to the
+      state budget, after public investment in the value chain.
+    </p>
 
-        <NiceMetricGroup>
-            <NiceMetric label="Total value added" value="300 000 000 &euro;"/>
-            <NiceMetric label="Value added share of the agricultural sector GDP" value="1.9 %"/>
-            <NiceMetric label="Value added share of national GDP" value="0.3 %"/>
-        </NiceMetricGroup>
+    <NiceMetricGroup>
+      <NiceMetric label="Total value added" value="300 000 000 &euro;" />
+      <NiceMetric label="Value added share of the agricultural sector GDP" value="1.9 %" />
+      <NiceMetric label="Value added share of national GDP" value="0.3 %" />
+    </NiceMetricGroup>
 
+    <h3>Is the chain independent from foreign imports?</h3>
 
-        <h3>Is the chain independent from foreign imports?</h3>
+    <p>Rate of integration ito domestic economy</p>
+    <p>= total value added / value of production</p>
 
-        <p>Rate of integration ito domestic economy</p>
-        <p>= total value added / value of production</p>
+    <h2>Who <strong>creates and receives</strong> value added?</h2>
 
-        <h2>Who <strong>creates and receives</strong> value added?</h2>
+    <h2>
+      How <strong>profitable</strong> and viable are the value chain activities for the actors
+      involved?
+    </h2>
+    <p>
+      RETURN ON INVESTMENT (%) = percentage of net operating profit in total costs (NB : for
+      producers this includes actor revenue)
+    </p>
+    <BarChart v-if="studyData" :options="populatedBarChartData"></BarChart>
+    <a v-else class="TODO">Data are missing to display this GraphBar</a>
 
-        <h2>How <strong>profitable</strong> and viable are the value chain activities for the actors involved?</h2>
-        <p>RETURN ON INVESTMENT (%) = percentage of net operating profit in total costs (NB : for producers this includes actor revenue)</p>
-        <BarChart v-if="studyData" :options="populatedBarChartData"></BarChart>
-        <a v-else class="TODO">Data are missing to display this GraphBar</a>
+    <h2>What is the contribution of the value chain to the <strong>balance of trade</strong>?</h2>
 
-        <h2>What is the contribution of the value chain to the <strong>balance of trade</strong>?</h2>
-
-        <h2>Is the value chain <strong>viable in the international economy</strong>?</h2>
-        <p>The VCA4D methodology assesses for each value chain its dependency on international exports as well as its capacity to export on international markets with competitive price or on the contrary a higher remuneration of the actors supported by porection policies.</p>
-
-    </article>
-
+    <h2>Is the value chain <strong>viable in the international economy</strong>?</h2>
+    <p>
+      The VCA4D methodology assesses for each value chain its dependency on international exports as
+      well as its capacity to export on international markets with competitive price or on the
+      contrary a higher remuneration of the actors supported by porection policies.
+    </p>
+  </article>
 </template>
 
 <script setup>
-import { computed, ref } from 'vue';
+import { computed, ref } from 'vue'
 
-import NiceMetricGroup from './NiceMetricGroup.vue';
-import NiceMetric from './NiceMetric.vue';
-import BarChart from './BarChart.vue';
-import Utils from '@/utils/utils.js';
-import CurrencyUtils from '@/utils/currencyUtils.js';
+import NiceMetricGroup from './NiceMetricGroup.vue'
+import NiceMetric from './NiceMetric.vue'
+import BarChart from './BarChart.vue'
+import Utils from '@/utils/utils.js'
+import CurrencyUtils from '@/utils/currencyUtils.js'
 
 const props = defineProps({
   studyData: Object
-});
+})
 
 const populatedBarChartData = computed(() => {
-    var categories = []
-    var values = []
-    var tooltip = {}
+  var categories = []
+  var values = []
+  var tooltip = {}
+  Utils.sortDataStudyArrays(props.studyData.data)
 
-    for (var row of props.studyData.data["Indicator by actor type"]) {
-        categories.push(row["Actor type Name"])
-        var stageName = Utils.getCorrespondingStage(row["Actor type Name"], props.studyData.data)
-        const v1 = row['Net operating profit (local currency)'];
-        var v2 = row['Total costs (local currency)'];
-        if (stageName == "Producers") {
-            v2 = v1 + v2 // Profits are considered as cost for producer while computing Return On Investment
-        }
-        values.push(100*v1/v2);
-        tooltip[row["Actor type Name"]] = `Net operating profit = ${CurrencyUtils.formatAmount(v1)}<br>
-                                           Total costs = ${CurrencyUtils.formatAmount(v2)}<br>
-                                           Return on investment = ${CurrencyUtils.formatPercent(100*v1/v2)}`
+  for (var row of props.studyData.data['Indicator by actor type']) {
+    categories.push(row['Actor type name'])
+    var stageName = Utils.getCorrespondingStage(row['Actor type name'], props.studyData.data)
+    const v1 = row['Net operating profit (local currency)']
+    var v2 = row['Total costs (local currency)']
+    if (stageName == 'Producers') {
+      v2 = v1 + v2 // Profits are considered as cost for producer while computing Return On Investment
     }
-    let result = {
-        xAxis: {
-            type: 'category',
-            boundaryGap: true,
-            axisTick: {
-                alignWithLabel: true
-            },
-            axisLabel: {
-                interval: 0,
-                rotate: -10,
-                margin: 20
-            },
-            data: categories
-        },
-        tooltip: {
-          trigger: "item",
-          formatter: function (info) {
-            return tooltip[info.name];
-          }
-        },
-        yAxis: {
-            type: 'value',
-            name: 'RETURN ON INVESTMENT (%)',
-            axisLine : {
-                show: true,
-                symbol: ['arrow', 'arrow']
-            }
-        },
-        series: {
-            type: 'bar',
-            data: values
+    values.push(((100 * v1) / v2).toFixed(0))
+    tooltip[row['Actor type name']] = `Net operating profit = ${CurrencyUtils.formatAmount(v1)}<br>
+                                           Total costs = ${CurrencyUtils.formatAmount(v2)}<br>
+                                           Return on investment = ${CurrencyUtils.formatPercent(
+                                             (100 * v1) / v2
+                                           )}`
+  }
+  let result = {
+    xAxis: {
+      type: 'category',
+      boundaryGap: true,
+      axisTick: {
+        alignWithLabel: true
+      },
+      axisLabel: {
+        interval: 0,
+        rotate: -10,
+        margin: 20
+      },
+      data: categories
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: function (info) {
+        return tooltip[info.name]
+      }
+    },
+    yAxis: {
+      type: 'value',
+      name: 'RETURN ON INVESTMENT (%)',
+      axisLine: {
+        show: true,
+        symbol: ['arrow', 'arrow']
+      }
+    },
+    series: {
+      type: 'bar',
+      data: values,
+      colorBy: 'data',
+      label: {
+        show: true,
+        position: 'top'
+      },
+      itemStyle: {
+        color: function (info) {
+          return Utils.getStageColor(Utils.getCorrespondingStage(info.name, props.studyData.data))
         }
-    };
-    console.log("result:", result);
-    return result;
-});
+      }
+    }
+  }
+  console.log('result:', result)
+  return result
+})
 </script>
 
 <style scoped lang="scss">
 @import '../../style/colors.scss';
 
 article.study-section {
+  h1 {
+    text-decoration: underline;
+    text-decoration-thickness: 3px;
+    font-weight: normal;
+    font-size: 24px;
+    color: #151515;
+    text-underline-offset: 5px;
+  }
 
-    h1 {
-        text-decoration: underline;
-        text-decoration-thickness: 3px;
-        font-weight: normal;
-        font-size: 24px;
-        color: #151515;
-        text-underline-offset: 5px;
-    }
+  h2 {
+    font-weight: normal;
+    font-size: 22px;
+    color: #151515;
+  }
 
-    h2 {
-        font-weight: normal;
-        font-size: 22px;
-        color: #151515;
-    }
-
-    h3 {
-        padding-left: 80px;
-        font-weight: bold;
-        font-size: 17px;
-        text-decoration: underline;
-        text-underline-offset: 5px;
-        text-decoration-thickness: 3px;
-        text-decoration-color: #999;
-    }
+  h3 {
+    padding-left: 80px;
+    font-weight: bold;
+    font-size: 17px;
+    text-decoration: underline;
+    text-underline-offset: 5px;
+    text-decoration-thickness: 3px;
+    text-decoration-color: #999;
+  }
 }
-
 </style>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,18 @@
-import { isProxy, toRaw } from 'vue';
+var StagesColors = {
+    Producers: "#33A027",
+    Collectors: "#ECD736",
+    Processors: "#fbb02d",
+    Wholesalers: "#2932D7",
+    Retailers: "#384d48",
+    Exporters: "#b57ba6"
+}
+
+const getStageColor = (stageName) => {
+    if (stageName in StagesColors) {
+        return StagesColors[stageName]
+    }
+    return "#ff1100"
+}
 
 const getCorrespondingStage = (actorTypeName, studyData) => {
     for (var stageDescription of studyData["Actor types"]) {
@@ -12,25 +26,23 @@ const getCorrespondingStage = (actorTypeName, studyData) => {
 var StagesWithOrder = {}
 var ActorsWithOrder = {}
 
-var Inconsistencies = []
+var Inconsistencies = new Set()
 
 const compareActor = (actorA, actorB) => {
     if (actorA["Stage"] in StagesWithOrder && actorB["Stage"] in StagesWithOrder) {
         if (StagesWithOrder[actorA["Stage"]] < StagesWithOrder[actorB["Stage"]]) {
-            console.log(actorA["Stage"], "beats", actorB["Stage"])
             return -1;
         }
         if (StagesWithOrder[actorA["Stage"]] > StagesWithOrder[actorB["Stage"]]) {
-            console.log(actorB["Stage"], "beats", actorA["Stage"])
             return 1;
         }
     }
-    else if(actorA["Stage"] in StagesWithOrder) {
-        Inconsistencies.push(`Stage '${actorA["Stage"]} of actor ${actorA["Actor type name"]} is not defined in Stage description`)
+    else if (actorA["Stage"] in StagesWithOrder) {
+        Inconsistencies.add(`Stage '${actorB["Stage"]}' of actor ${actorB["Actor type name"]} is not defined in Stage description`)
         return -1
     }
-    else if(actorB["Stage"] in StagesWithOrder) {
-        Inconsistencies.push(`Stage '${actorB["Stage"]} of actor ${actorB["Actor type name"]} is not defined in Stage description`)
+    else if (actorB["Stage"] in StagesWithOrder) {
+        Inconsistencies.add(`Stage '${actorA["Stage"]}' of actor ${actorA["Actor type name"]} is not defined in Stage description`)
         return 1
     }
     // a doit être égal à b
@@ -40,20 +52,18 @@ const compareActor = (actorA, actorB) => {
 const compareRow = (rowA, rowB) => {
     if (rowA["Actor type name"] in ActorsWithOrder && rowB["Actor type name"] in ActorsWithOrder) {
         if (ActorsWithOrder[rowA["Actor type name"]] < ActorsWithOrder[rowB["Actor type name"]]) {
-            console.log(rowA["Actor type name"], "beats", rowB["Actor type name"])
             return -1;
         }
         if (ActorsWithOrder[rowA["Actor type name"]] > ActorsWithOrder[rowB["Actor type name"]]) {
-            console.log(rowB["Actor type name"], "beats", rowA["Actor type name"])
             return 1;
         }
     }
-    else if(rowA["Actor type name"] in ActorsWithOrder) {
-        Inconsistencies.push(`Actor '${rowA["Actor type name"]} is not defined in Actors description`)
+    else if (rowA["Actor type name"] in ActorsWithOrder) {
+        Inconsistencies.add(`Actor '${rowB["Actor type name"]}' is not defined in Actors description`)
         return -1
     }
-    else if(rowB["Actor type name"] in ActorsWithOrder) {
-        Inconsistencies.push(`Actor '${rowB["Actor type name"]} is not defined in Actors description`)
+    else if (rowB["Actor type name"] in ActorsWithOrder) {
+        Inconsistencies.add(`Actor '${rowA["Actor type name"]}' is not defined in Actors description`)
         return 1
     }
     // a doit être égal à b
@@ -61,14 +71,14 @@ const compareRow = (rowA, rowB) => {
 }
 
 const sortDataStudyArrays = (dataStudy) => {
-    // Get order of stages
+    Inconsistencies = new Set()
+    // Get order of stagesProcessors
     StagesWithOrder = {}
     var order = 1
     for (var stageDescription of dataStudy["Stages description"]) {
         StagesWithOrder[stageDescription["Stages"]] = order
         order++
     }
-    console.log("orders", StagesWithOrder)
     dataStudy["Actor types"].sort(compareActor)
 
     // Define actors order based on stages order
@@ -77,8 +87,7 @@ const sortDataStudyArrays = (dataStudy) => {
     for (var actorDescription of dataStudy["Actor types"]) {
         ActorsWithOrder[actorDescription["Actor type name"]] = order
         order++
-    }    
-    console.log("orders", ActorsWithOrder)
+    }
 
     // sort dataStudy content
     dataStudy["Indicator by actor type"].sort(compareRow)
@@ -87,5 +96,6 @@ const sortDataStudyArrays = (dataStudy) => {
 
 export default {
     getCorrespondingStage,
+    getStageColor,
     sortDataStudyArrays
 };
