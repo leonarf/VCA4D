3e5d4a445b9bd78e2b0d8debf999e08fd535880b
Tristan Gueguen <tristan.gueguen@gmail.com>
2023-09-22 15:54:30 +0200

    fix(social): removed comments hover

--- src/components/EvaluationCriterionLevel3.vue
+++ src/components/EvaluationCriterionLevel3.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <div class="flex flex-row items-start w-full my-3" :title="comment">
+    <div class="flex flex-row items-start w-full my-3">
       <div class="bg-[#8a8a8a] text-white py-0 px-3 mr-3 font-bold rounded-2xl">{{ counter }}</div>
       <div class="flex-grow text-justify mx-3">{{ label }}</div>
       <EvaluationCriterionLevel3ScaleTag :scale="scale" :appreciation="appreciation" />
