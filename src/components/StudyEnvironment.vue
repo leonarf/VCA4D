9e362d138c4feacb32212e0e155e751b4b9f8f4b
Tristan Gueguen <tristan.gueguen@gmail.com>
2023-11-22 18:17:52 +0100

    PT/Other button, display results accordingly

--- src/components/StudyEnvironment.vue
+++ src/components/StudyEnvironment.vue
@@ -10,6 +10,25 @@
       specific policies or project may be developped in order to improve the sustainability of a
       step or of some actors to improve the overall sustainability of the value chain.
     </p>
+    <div class="flex flex-row justify-center gap-x-4">
+        <label v-for="unit in units" :key="unit.value"
+        :class="{
+          'inline-block w-40 h-10 text-center py-2 border-2 rounded cursor-pointer text-black text-md': true,
+          'border-blue-200 bg-blue-100': selectedUnit === unit.value,
+          'border-gray-200 bg-gray-100': selectedUnit !== unit.value 
+        }" 
+          >
+          <input
+          type="radio"
+          :id="unit.value"
+          :value="unit.value"
+          v-model="selectedUnit"
+          name="radioGroup"
+          class="hidden"
+          />
+          {{ unit.label }}
+        </label>
+    </div>
     <template v-if="studyData">
       <div v-for="impact in allBarChartsData" :key="impact.name">
         <ImpactDataviz :impact="impact" />
@@ -19,7 +38,7 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import SectionTitle from '@typography/SectionTitle.vue'
 import ImpactDataviz from '@/components/study/environment/ImpactDataviz.vue'
 import { ACVImpacts } from '@/utils/misc.js'
@@ -31,11 +50,22 @@ const props = defineProps({
 const allBarChartsData = computed(() => {
   var majorImpactsNames = ACVImpacts.map((impact) => impact.name)
   var impactsToDrawOnGraph = props.studyData.acvData.impacts.filter((impact) => {
-    return majorImpactsNames.includes(impact.name)
-  })
+    if (!majorImpactsNames.includes(impact.name)) {
+      return false
+    }
+    return selectedUnit.value === "PT" ? (impact.unit === 'Pt') : (impact.unit !== 'Pt')
+    })
 
   return impactsToDrawOnGraph
 })
+
+const units = [
+  { label: 'Pt', value: 'PT' },
+  { label: 'Autre unités', value: 'OTHER' },
+];
+
+const selectedUnit = ref(units[0].value); 
+
 </script>
 
 <style scoped lang="scss"></style>
