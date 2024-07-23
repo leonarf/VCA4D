ec66aa349c4b7ec6f474da3a9ea9842908b61547
maximepiard <maxime@lebasic.com>
2024-07-23 17:46:55 +0200

    Create wrapper component for SVG
    I'm adding a vite plugin for this, as proposed in  https://mkay11.medium.com/heres-a-simple-solution-to-incorporate-inline-svgs-in-vue-vite-da5897a480f7

---
+++ src/components/Svg.vue
@@ -0,0 +1,16 @@
+<template>
+  <component 
+    :is="svg" 
+    class="svg"
+  />
+</template>
+
+<script setup>
+defineProps({
+  svg: Function
+});
+</script>
+
+<style scoped lang="scss">
+
+</style>
