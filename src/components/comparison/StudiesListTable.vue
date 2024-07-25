7ad91c2f6679ddc4a371f8786f95a1d18fc78fa2
maximepiard <maxime@lebasic.com>
2024-07-25 10:30:02 +0200

    feat [table 2/3]: Allow to select studies based on initial list

--- src/components/comparison/StudiesListTable.vue
+++ src/components/comparison/StudiesListTable.vue
@@ -1,5 +1,6 @@
 <template>
   <TableLite
+    :is-slot-mode="true"
     class="table"
     :isLoading="studies.length === 0"
     :columns="columns"
@@ -8,7 +9,15 @@
     :rows="studies"
     :page-size="50"
     :is-hide-paging="studies.length <= 50"
+    @row-clicked="(rowData) => toggleRowSelection(rowData.id)"
   >
+    <template v-slot:checkbox="{ value: studyData }">
+      <input
+        type="checkbox"
+        :checked="isSelected(studyData.id)"
+        @click.stop="toggleRowSelection(studyData.id)"
+      />
+    </template>
   </TableLite>
 </template>
 
@@ -18,14 +27,36 @@
   import TableLite from 'vue3-table-lite';
   import { getAllJsonData, getStudy, getStudyData, getProduct, getCountry } from '@utils/data';
 
+  const props = defineProps({
+    selectedStudies: Array
+  });
   const studies = ref([]);
+  const newSelectedStudies = ref([]);
 
   onMounted(async () => {
-    const jsonStudies = getAllJsonData().studies;
-    studies.value = await Promise.all(jsonStudies.map(({ id }) => getStudyData(id)))
+    newSelectedStudies.value = props.selectedStudies;
+    studies.value = await loadStudyData();
+
+    async function loadStudyData() {
+      const jsonStudies = getAllJsonData().studies;
+      return Promise.all(jsonStudies.map(({ id }) => getStudyData(id)))
+    }
   });
 
+  function isSelected(studyId) {
+    return newSelectedStudies.value.includes(studyId);
+  }
+  function toggleRowSelection(studyId) {
+    if (newSelectedStudies.value.includes(studyId)) {
+      newSelectedStudies.value = newSelectedStudies.value.filter(id => id !== studyId);
+    } else {
+      newSelectedStudies.value.push(studyId)
+    }
+  }
+
   const columns = [{
+    field: "checkbox"
+  }, {
     label: "Product",
     display: (studyData) => _.capitalize(getProduct(getStudy(studyData.id).product).prettyName)
   }, {
@@ -48,5 +79,8 @@
 </script>
 
 <style scoped lang="scss">
+.table {
+  overflow-y: scroll;
+}
 </style>
 
