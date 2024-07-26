efb97a9a114171c2e6744d10e47ec5da0e4ee32e
maximepiard <maxime@lebasic.com>
2024-07-26 10:50:17 +0200

    boyscout : Migrate to composition API

--- src/components/charts/TableauDeDonnées.vue
+++ src/components/charts/TableauDeDonnées.vue
@@ -2,55 +2,44 @@
   <div style="width: 100%">
     <TableLite
       :is-static-mode="true"
-      :is-loading="table.isLoading"
       :columns="tableContent.columns"
       :rows="tableContent.rows"
       :total="tableContent.totalRecordCount"
       :sortable="table.sortable"
       :messages="table.messages"
       :is-hide-paging="tableContent.totalRecordCount <= 10"
-      @is-finished="table.isLoading = false"
     >
     </TableLite>
   </div>
 </template>
 
-<script>
-import TableLite from "vue3-table-lite";
+<script setup>
+  import { ref, computed } from "vue";
+  import TableLite from "vue3-table-lite";
 
-export default {
-  components: {
-    TableLite,
-  },
-  props: {
-    données: Array,
-  },
-  data() {
-    return {
-      table: {
-        isLoading: false,
-        sortable: {
-          order: "id",
-          sort: "asc",
-        },
-      },
-    };
-  },
-  computed: {
-    tableContent() {
-      var columns = Object.keys(this.données[0]).map((property) => {
-        return {
-          label: property,
-          field: property,
-          sortable: true,
-        };
-      });
-      return {
-        columns,
-        rows: this.données,
-        totalRecordCount: this.données.length,
-      };
+  const props = defineProps({
+    données: Array
+  });
+  const table = ref({
+    isLoading: false,
+    sortable: {
+      order: "id",
+      sort: "asc",
     },
-  },
-};
+  });
+
+  const tableContent = computed(() => {
+    var columns = Object.keys(props.données[0]).map((property) => {
+      return {
+        label: property,
+        field: property,
+        sortable: true,
+      };
+    });
+    return {
+      columns,
+      rows: props.données,
+      totalRecordCount: props.données.length,
+    };
+  });
 </script>
