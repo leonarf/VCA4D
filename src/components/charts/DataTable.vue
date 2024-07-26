1034c8f0b188ff0670a3691efcfc338fc767193a
maximepiard <maxime@lebasic.com>
2024-07-26 11:39:58 +0200

    refactor: Allow dataTable to select rows
    I'm basing myself on `row.id` at every time, the array items need to have an id field for this.

--- src/components/charts/DataTable.vue
+++ src/components/charts/DataTable.vue
@@ -1,23 +1,68 @@
 <template>
   <div style="width: 100%">
     <TableLite
-      :is-static-mode="true"
-      :columns="columns"
+      :is-slot-mode="true"
+      :columns="columnsWithCheckbox"
       :rows="props.rows"
       :total="props.rows.length"
       :sortable="true"
       :is-hide-paging="props.rows.length <= 10"
+      @row-clicked="onClickRow"
     >
+      <template v-if="selectable" v-slot:checkbox="{ value: rowData }">
+        <input
+          type="checkbox"
+          :checked="isSelected(rowData.id)"
+          @click.stop="toggleRowSelection(rowData.id)"
+        />
+      </template>
     </TableLite>
   </div>
 </template>
 
 <script setup>
-  import { ref, computed } from "vue";
+  import { computed } from "vue";
   import TableLite from "vue3-table-lite";
 
   const props = defineProps({
+    selectable: Boolean,
+    selectedIds: {
+      type: Array,
+      default: []
+    },
     rows: Array,
     columns: Array
   });
+
+  const emits = defineEmits(["update:selectedIds"]);
+
+  function isSelected(rowId) {
+    return props.selectedIds.includes(rowId);
+  }
+
+  function onClickRow(rowData) {
+    if (! props.selectable) { return; }
+    return toggleRowSelection(rowData.id)
+  }
+
+  function toggleRowSelection(rowId) {
+    let newSelectedIds = [...props.selectedIds];
+
+    if (props.selectedIds.includes(rowId)) {
+      newSelectedIds = newSelectedIds.filter(id => id !== rowId);
+    } else {
+      newSelectedIds.push(rowId)
+    }
+
+    emits("update:selectedIds", newSelectedIds);
+  }
+
+  const columnsWithCheckbox = computed(() => {
+    if (! props.selectable) { return props.columns; }
+
+    return [
+      { field: "checkbox" },
+      ...props.columns
+    ];
+  })
 </script>
