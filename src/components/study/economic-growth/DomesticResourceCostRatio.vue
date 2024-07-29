ec75117fe1c4f87bdc12a05ba59336d327bd231c
leonard <leonard@lebasic.com>
2024-07-29 15:14:11 +0200

    remplacement de VC par Value Chain + reformatage des fichiers

--- src/components/study/economic-growth/DomesticResourceCostRatio.vue
+++ src/components/study/economic-growth/DomesticResourceCostRatio.vue
@@ -1,25 +1,23 @@
 <template >
-    <Gauge 
-        :value="value" 
-        :minValue="0" 
-        :maxValue="2" 
-        :ticks="[0, 0.5, 1, 1.5, 2]"
-        :hasBackground="false"
-        :texts="[
-            '<b>Viable in the global economy :</b><br /> the cost of domestic factors (labour, capital, land, environmental goods) is lower than the value they produce.',
-            '<b>Considered not viable within the gobal economy :</b><br /> inefficient use of domestic resources, poor remuneration of domestic factors, this a non-competitive VC into the international economy.'
-        ]"
-        />
+  <Gauge
+    :value="value"
+    :minValue="0"
+    :maxValue="2"
+    :ticks="[0, 0.5, 1, 1.5, 2]"
+    :hasBackground="false"
+    :texts="[
+      '<b>Viable in the global economy :</b><br /> the cost of domestic factors (labour, capital, land, environmental goods) is lower than the value they produce.',
+      '<b>Considered not viable within the gobal economy :</b><br /> inefficient use of domestic resources, poor remuneration of domestic factors, this a non-competitive Value Chain into the international economy.'
+    ]"
+  />
 </template>
 
 <script setup>
-import Gauge from '@charts/Gauge.vue';
+import Gauge from '@charts/Gauge.vue'
 const props = defineProps({
-  value: Number,
+  value: Number
 })
-
 </script>
 
 <style>
-    
 </style>
