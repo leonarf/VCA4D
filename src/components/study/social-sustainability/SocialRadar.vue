100a6ac7160c1ad3db0563b4e0e300f8433a4ab4
leonard <leonard@lebasic.com>
2024-07-29 12:00:21 +0200

    déplacement 1 fichier excel + ajout lien comparaison

--- src/components/study/social-sustainability/SocialRadar.vue
+++ src/components/study/social-sustainability/SocialRadar.vue
@@ -1,15 +1,15 @@
 <template>
-    <Radar :options="chartData" />
+  <Radar :options="chartData" />
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed } from 'vue'
 import { getSocialAverageGroup } from '@utils/misc.js'
 import { getTagColor } from '@utils/colors.js'
 
 import Radar from '@charts/Radar.vue'
 const props = defineProps({
-    studyData: Object
+  studyData: Object
 })
 
 const averageWorking = computed(() => getSocialAverageGroup(props.studyData.socialData, 0))
@@ -20,78 +20,83 @@ const averageSocial = computed(() => getSocialAverageGroup(props.studyData.socia
 const averageLiving = computed(() => getSocialAverageGroup(props.studyData.socialData, 5))
 
 const chartData = computed(() => {
-    const values = [averageWorking.value, averageLiving.value, averageSocial.value, averageFood.value, averageGender.value, averageLand.value]
+  const values = [
+    averageWorking.value,
+    averageLiving.value,
+    averageSocial.value,
+    averageFood.value,
+    averageGender.value,
+    averageLand.value
+  ]
 
-    return {
-        radar: {
-            indicator: [
-                { name: '', max: 4 },
-                { name: '', max: 4 },
-                { name: '', max: 4 },
-                { name: '', max: 4 },
-                { name: '', max: 4 },
-                { name: '', max: 4 }
-            ],
-            center: ['50%', '50%'],
-            radius: '100%',
-            splitNumber: 4,
-            splitArea: {
-                areaStyle: {
-        color: [
-            getTagColor(1),
-            getTagColor(2),
-            getTagColor(3),
-            getTagColor(4),
-        ],
+  return {
+    radar: {
+      indicator: [
+          { name: 'Working conditions', max: 4 },
+          { name: 'Living conditions', max: 4 },
+          { name: 'Social capital', max: 4 },
+          { name: 'Food & nutrition security', max: 4 },
+          { name: 'Gender equality', max: 4 },
+          { name: 'Land & water rights', max: 4 },
+      ],
+      center: ['50%', '50%'],
+      radius: '100%',
+      splitNumber: 4,
+      splitArea: {
+        areaStyle: {
+          color: [getTagColor(1), getTagColor(2), getTagColor(3), getTagColor(4)]
+        }
       },
-            },
-            axisLine: {
-                lineStyle: {
-                    color: '#941B33'
-                }
-            },
-            axisLabel: {
-                show: false,
-            },
-            axisName: {
-                fontWeight: 'bold',
-                fontSize: '18px'
+      axisLine: {
+        lineStyle: {
+          color: '#941B33'
+        }
+      },
+      axisLabel: {
+        show: false
+      },
+      axisName: {
+        show: false,
+        formatter: function (value, param, other) {
+            console.log("value", value, param)
+          return value
         },
-        },
-        tooltip: {
-            show: false,
-            trigger: 'item',
-            formatter: function () {
-                return `Working: ${averageWorking.value}<br />
+        fontWeight: 'bold',
+        fontSize: '18px'
+      }
+    },
+    tooltip: {
+      show: false,
+      trigger: 'item',
+      formatter: function () {
+        return `Working: ${averageWorking.value}<br />
                 Living: ${averageLiving.value}<br />
                 Social: ${averageSocial.value}<br />
                 Food: ${averageFood.value}<br />
                 Gender: ${averageGender.value}<br />
                 Land: ${averageLand.value}<br />
-                `;
-            }
+                `
+      }
+    },
+    series: [
+      {
+        type: 'radar',
+        areaStyle: {
+          color: '#FFFFFF66'
         },
-        series: [
-            {
-                type: 'radar',
-                areaStyle: {
-                    color: "#FFFFFF66"
-                },
-                itemStyle: {
-                    color: "#941B33"
-                },
-                data: [
-                    {
-                        value: values,
-                    }
-                    
-                ]
-            }
-        ],
-    };
+        itemStyle: {
+          color: '#941B33'
+        },
+        data: [
+          {
+            value: values
+          }
+        ]
+      }
+    ]
+  }
 })
 </script>
 
 <style lang="">
-    
 </style>
