129e214aaed4070ed634991e5525baadc1803b12
maximepiard <maxime@lebasic.com>
2024-08-02 09:45:50 +0200

    prep: Copy-paste Tippty tooltip from monrepo
    Note: I'm removing the custom couleur options which is related to the monorepo DS colors

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import tippy from "tippy.js";
+import type { Instance, Props } from "tippy.js";
+import { onMounted, onUnmounted, onUpdated, ref } from "vue";
+import "tippy.js/dist/tippy.css";
+
+interface Tooltip {
+  contenu: string;
+  options: Partial<Props>;
+}
+
+const props = defineProps<Tooltip>();
+
+// Setup tooltip
+const tooltipRef = ref<HTMLElement>();
+
+const tooltipInstance = ref<Instance>();
+
+function destroyTooltip() {
+  if (tooltipInstance.value) {
+    tooltipInstance.value.destroy();
+  }
+}
+
+function initTooltip() {
+  destroyTooltip();
+  tooltipInstance.value = tippy(tooltipRef.value?.parentNode as HTMLElement, {
+    content: props.contenu,
+    allowHTML: true,
+    interactive: true,
+    ...props.options,
+    onMount(instance) {
+      const bgColor = "#EDEDED";
+      (
+        instance.popper.querySelector(".tippy-box") as HTMLElement
+      ).style.backgroundColor = bgColor;
+      const textColor = "#6B6B6B";
+      (instance.popper.querySelector(".tippy-box") as HTMLElement).style.color =
+        textColor;
+      const arrowElement = instance.popper.querySelector(
+        ".tippy-arrow"
+      ) as HTMLElement;
+      arrowElement.style.color = bgColor;
+    },
+  });
+}
+
+onMounted(initTooltip);
+onUpdated(initTooltip);
+onUnmounted(destroyTooltip);
+</script>
+
+<template>
+  <span ref="tooltipRef"></span>
+</template>
+
+<style></style>
