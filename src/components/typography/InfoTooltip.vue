cf5b32ccf271853657ae914d30e9616b29ccd6db
maximepiard <maxime@lebasic.com>
2024-08-21 10:48:04 +0200

    refactor: Extract infotooltip on question mark

---
+++ src/components/typography/InfoTooltip.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Svg
+      class="question-mark"
+      :svg="QuestionMark"
+    />
+    <Tooltip
+      :contenu="text"
+      :options="{ placement: 'right', maxWidth: 350 }"
+    />
+  </div>
+</template>
+
+<script setup>
+import Tooltip from "@components/Tooltip.vue";
+import Svg from "@components/Svg.vue";
+import QuestionMark from '../../images/icons/info-question.svg'
+
+const props = defineProps({
+  text: String,
+})
+</script>
+
+<style scoped lang="scss">
+  .question-mark {
+    height: 1em;
+  }
+</style>
