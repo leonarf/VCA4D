0a110ee39fdc34ec169495fc071b8c193894a3b2
maximepiard <maxime@lebasic.com>
2024-08-27 08:23:09 +0200

    feat [setup 2/3]: Update eslint config
    Notes:
    
    - I'm removing prettier for now, we'll see if including it changes a lot later
    - I'm disabling every vue rule we don't respect, to fix them one by one next commits

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,55 @@
 
 import js from "@eslint/js";
 import pluginVue from 'eslint-plugin-vue';
+import vueParser from 'vue-eslint-parser';
 
 export default [
   js.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
+  {
+    files: ["**/*.vue", "**/*.js"],
+    rules: {
+      "no-extra-boolean-cast":"off",
+      "no-unused-vars": "off",
+      "no-undef": "off",
+      "no-case-declarations": "off",
+      "no-redeclare": "off",
+    }
+  },
+  {
+    files: ["**/*.vue"],
+    plugins: {
+      vue: pluginVue,
+    },
+    languageOptions: { parser: vueParser },
+    rules: {
+      // Will fix in a few commits
+      "vue/no-v-html": "off",
+      "vue/require-explicit-emits": "off",
+      "vue/attribute-hyphenation": "off",
+      "vue/max-attributes-per-line": "off",
+      "vue/first-attribute-linebreak": "off",
+      "vue/attributes-order": "off",
+      "vue/no-multi-spaces": "off",
+      "vue/singleline-html-element-content-newline": "off",
+      "vue/html-self-closing": "off",
+      "vue/html-closing-bracket-spacing": "off",
+      "vue/html-closing-bracket-newline": "off",
+      "vue/component-tags-order": "off",
+      "vue/multiline-html-element-content-newline": "off",
+      "vue/v-on-event-hyphenation": "off",
+      "vue/v-slot-style": "off",
+      "vue/v-bind-style": "off",
+      "vue/mustache-interpolation-spacing": "off",
+      "vue/html-quotes": "off",
+      "vue/html-end-tags": "off",
+      "vue/no-parsing-error": "off",
+      "vue/valid-v-for": "off",
+      "vue/require-valid-default-prop": "off",
+      "vue/html-indent": "off",
+      "no-mixed-spaces-and-tabs": "off",
+      "vue/multi-word-component-names":"off",
+      "vue/require-default-prop": "off",
+    }
+  }
 ]
