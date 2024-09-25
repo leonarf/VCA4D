1dd0b4062317d05299fc2a71aa475faccbd22a22
maximepiard <maxime@lebasic.com>
2024-09-25 09:48:12 +0200

    feat :three: [3/3]: Add 2 expandable rows that show per total year / per functional unit
    Note: When computing the impact values, I should still multiply the volume produced by subchains, then divide the totalVolume at the end for the "By functional unit" part. Because otherwise we'll just sum the average impacts 'per functional unit' for all subchains, and that's not right, volumes have to be taken into account for the right average on the whole chain

--- src/components/comparison/ComparisonEnvironment.vue
+++ src/components/comparison/ComparisonEnvironment.vue
@@ -1,27 +1,37 @@
 <template>
   <template v-if="impacts.length > 0">
     <ComparisonTitle title="Environmental Indicators" :studies="studies" />
-    <ComparisonRow
-      v-for="impact in impacts"
-      :key="`impact_${impact.name}`"
+    <ComparisonExpandableRow
       :studies="studies"
-      :title="impact.name"
-      :subtitle="`in ${getUnitImpact(impact.name)}`"
-      :getValue="(study) => getImpactValue(impact, study)"
+      title="Total impact per year"
+      subtitle="Value chain total impact (in pt)"
+      :getValue="(study) => getTotalImpacts(study, true)"
+      :getSubValues="(study) => getValuesByImpact(study, true)"
     >
       <template #default="{ value }">
         <ComparisonDefaultCell :value="value" valueType="number" />
       </template>
-    </ComparisonRow>
+    </ComparisonExpandableRow>
+    <ComparisonExpandableRow
+      :studies="studies"
+      title="Impact per functional unit"
+      subtitle="Value chain total impact / volume"
+      :getValue="(study) => getTotalImpacts(study, false)"
+      :getSubValues="(study) => getValuesByImpact(study, false)"
+    >
+      <template #default="{ value }">
+        <ComparisonDefaultCell :value="value" valueType="number" />
+      </template>
+    </ComparisonExpandableRow>
   </template>
 </template>
 
 <script setup>
-
+import _ from "lodash";
 import { ACVImpacts } from '@utils/misc.js'
 import { computed } from 'vue';
 import ComparisonTitle from './ComparisonTitle.vue';
-import ComparisonRow from './ComparisonRow.vue';
+import ComparisonExpandableRow from './ComparisonExpandableRow.vue';
 import ComparisonDefaultCell from './ComparisonDefaultCell.vue';
 const props = defineProps({
     studies: Array,
@@ -34,24 +44,42 @@ const availableImpacts = computed(() => props.studies.reduce((arr, study) => arr
 
 const impacts = computed(() => ACVImpacts.filter(item => availableImpacts.value.map(availableImpact => availableImpact.name).includes(item.name)))
 
-const getUnitImpact = (impactName) => availableImpacts.value.find(impact => impact.name === impactName).unit
+function getTotalImpacts(study, sumTotalPerYear = false) {
+  const valuesByImpact = getValuesByImpact(study, sumTotalPerYear);
+  if (_.isEmpty(valuesByImpact)) { return null; }
 
-const getImpactValue = (impact, study) => {
-    if (!study.acvData) {
-        return null;
-    }
-    const valueChains = study.acvData.valuechains
-    let total = 0
-    for (const { name, volume} of valueChains) {
-        const totalChainPerT = study.acvData.impacts
-            .filter(i => i.name === impact.name)
-            .filter(i => i.unit === 'Pt')
-            .reduce((arr, item) => arr.concat(item.values), [])
-            .filter(val => val.valuechain_name === name)
-            .map(val => val.value * volume).reduce((s, item) => s + item, 0)
-        total += totalChainPerT
-    }
-    return total
+  return _.sumBy(Object.values(valuesByImpact));
+}
+
+function getValuesByImpact(study, sumTotalPerYear = false) {
+  const valuesByImpact = {};
+  impacts.value.forEach((impact) => {
+    const impactValue = getImpactValue(impact, study, sumTotalPerYear);
+    if (_.isNull(impactValue)) { return; }
+
+    valuesByImpact[impact.name] = impactValue;
+  });
+  return valuesByImpact;
+}
+
+function getImpactValue(impact, study, sumTotalPerYear = false) {
+  if (!study.acvData) {
+      return null;
+  }
+  const valueChains = study.acvData.valuechains
+  let totalPerYear = 0;
+  let totalVolume = 0;
+  for (const { name, volume} of valueChains) {
+    const totalChainPerYear = study.acvData.impacts
+      .filter(i => i.name === impact.name)
+      .filter(i => i.unit === 'Pt')
+      .reduce((arr, item) => arr.concat(item.values), [])
+      .filter(val => val.valuechain_name === name)
+      .map(val => val.value * volume).reduce((s, item) => s + item, 0)
+    totalVolume += volume;
+    totalPerYear += totalChainPerYear
+  }
+  return sumTotalPerYear ? totalPerYear : (totalPerYear / totalVolume)
 }
 
 </script>
