c48fa1c5ef8aaf5aa81cd59a3acd903501885b1c
Maxime Piard <maxime@lebasic.com>
2024-11-08 12:15:18 +0100

    feat: Change color to match exactly the excel colors (#137)
    ## Context
    
    The client wants the social colors to exactly match the saturated tones
    inside the excel files.

--- src/utils/social.js
+++ src/utils/social.js
@@ -1,46 +1,44 @@
 const SOCIAL_COLORS = {
-  NA: "#D1D5DB",
-  BadScoreRed: "#ff8c8c",
-  LowScoreOrange: "#ffdd75",
-  SubstantialScoreYellow: "#aee376",
-  HighScoreGreen : "#67db9c",
+  NA: '#D1D5DB',
+  BadScoreRed: '#ff0000',
+  LowScoreOrange: '#ffc000',
+  SubstantialScoreYellow: '#92d050',
+  HighScoreGreen: '#00b050'
 }
 
 const average = (array) => array.reduce((a, b) => a + b) / array.length
 
 export function getSocialAverageGroup(socialImpact) {
-  return Math.round(average(socialImpact.groups.map(group => Number(group.averageValue) || 0)), 2);
+  return Math.round(average(socialImpact.groups.map((group) => Number(group.averageValue) || 0)), 2)
 }
 
 const socialTagsConfig = {
-  na: { color: SOCIAL_COLORS["NA"], appreciation: "n/a" },
-  bad: { color: SOCIAL_COLORS["BadScoreRed"], appreciation: "Not at all" },
-  moderate: { color: SOCIAL_COLORS["LowScoreOrange"], appreciation: "Moderate/Low" },
-  substantial: { color: SOCIAL_COLORS["SubstantialScoreYellow"], appreciation: "Substantial" },
-  high: { color: SOCIAL_COLORS["HighScoreGreen"], appreciation: "High" },
-};
+  na: { color: SOCIAL_COLORS['NA'], appreciation: 'n/a' },
+  bad: { color: SOCIAL_COLORS['BadScoreRed'], appreciation: 'Not at all' },
+  moderate: { color: SOCIAL_COLORS['LowScoreOrange'], appreciation: 'Moderate/Low' },
+  substantial: { color: SOCIAL_COLORS['SubstantialScoreYellow'], appreciation: 'Substantial' },
+  high: { color: SOCIAL_COLORS['HighScoreGreen'], appreciation: 'High' }
+}
 
 export function getSocialScoreColor(value) {
-  return getSocialConfig(value).color;
+  return getSocialConfig(value).color
 }
 export function getSocialScoreLabel(value) {
-  return getSocialConfig(value).appreciation;
+  return getSocialConfig(value).appreciation
 }
 
 export function getSocialConfig(value) {
-  if (isNaN(value)) { return socialTagsConfig.na; }
+  if (isNaN(value)) {
+    return socialTagsConfig.na
+  }
 
   if (value < 1.5) {
     return socialTagsConfig.bad
-  }
-  else if (value < 2.5) {
+  } else if (value < 2.5) {
     return socialTagsConfig.moderate
-  }
-  else if (value < 3.5) {
+  } else if (value < 3.5) {
     return socialTagsConfig.substantial
-  }
-  else {
+  } else {
     return socialTagsConfig.high
   }
 }
-
