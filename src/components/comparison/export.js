0ddfe3571f99c4c16eb859bfbbba2efe119cb83b
maximepiard <maxime@lebasic.com>
2024-11-08 12:21:30 +0100

    feat: Add formatting for percetage vs numbers

--- src/components/comparison/export.js
+++ src/components/comparison/export.js
@@ -3,6 +3,7 @@ import { getAllSubKeys } from './utils'
 import { slugify } from '@utils/format.js'
 import { getCountry, getProduct } from '@utils/data'
 import * as XLSX from 'xlsx'
+import _ from 'lodash'
 
 export function downloadComparisonXlsx(studies) {
   const dataTable = [
@@ -15,27 +16,79 @@ export function downloadComparisonXlsx(studies) {
     ...unfoldIndicators(studies, comparisonConfig.environment)
   ]
 
-  const worksheet = XLSX.utils.aoa_to_sheet(dataTable)
+  const worksheet = buildSheet(dataTable)
   worksheet['!cols'] = [{ wch: 40 }, ...studies.map(() => ({ wch: 20 }))]
   const workbook = XLSX.utils.book_new()
   XLSX.utils.book_append_sheet(workbook, worksheet, 'Comparison data')
   XLSX.writeFile(workbook, 'Studies Comparison.xlsx', { compression: true })
 }
 
+function buildSheet(dataTable) {
+  const sheet = XLSX.utils.aoa_to_sheet([])
+  dataTable.forEach((row, rowIndex) => setRow(sheet, row, rowIndex))
+  return sheet
+}
+function setRow(sheet, row, rowIndex) {
+  const rowValues = row.map((cell) => cell.value)
+  XLSX.utils.sheet_add_aoa(sheet, [rowValues], {
+    origin: XLSX.utils.encode_cell({ c: 0, r: rowIndex })
+  })
+  const rowCells = getRow(sheet, rowIndex)
+
+  row.forEach((cellData, cellIndex) => {
+    if (!rowCells[cellIndex]) {
+      return
+    }
+    rowCells[cellIndex].z = getFormat(cellData)
+  })
+}
+function getFormat({ type = 'number', value }) {
+  switch (type) {
+    case 'string':
+      return ''
+    case 'number':
+      return value > 100 ? '#,##0' : '0.00'
+    case 'social':
+      return '0'
+    case 'percent':
+      return '0.00%'
+    // WIP will fix next commit
+    case 'amount':
+    default:
+      return '#,##0'
+  }
+}
+
+function getRow(sheet, rowIndex) {
+  const { columnIndex } = getSheetLastCellIndexes()
+  const rowCells = _.range(0, columnIndex + 1).map(
+    (column) => sheet[XLSX.utils.encode_cell({ c: column, r: rowIndex })]
+  )
+  return rowCells
+
+  function getSheetLastCellIndexes() {
+    const ref = sheet['!ref']
+    const { e: endCell } = XLSX.utils.decode_range(ref)
+    return {
+      rowIndex: endCell.r,
+      columnIndex: endCell.c
+    }
+  }
+}
 function getHeaderRows(studies) {
   const productRow = [
-    '',
-    ...studies.map((study) => getProduct(study?.commodity?.toLowerCase()).prettyName)
+    getCell(''),
+    ...studies.map((study) => getCell(getProduct(study?.commodity?.toLowerCase()).prettyName))
   ]
   const countryRow = [
-    '',
-    ...studies.map((study) => getCountry(slugify(study?.country))?.prettyName)
+    getCell(''),
+    ...studies.map((study) => getCell(getCountry(slugify(study?.country))?.prettyName))
   ]
   return [productRow, countryRow]
 }
 
 function getSectionTitleRow(title) {
-  return [title.toUpperCase()]
+  return [getCell(title.toUpperCase())]
 }
 
 function unfoldIndicators(studies, indicators) {
@@ -49,7 +102,10 @@ function unfoldIndicator(studies, indicator) {
 }
 
 function getMainRow(studies, indicator) {
-  return [indicator.title, ...studies.map((study) => indicator.getValue(study, studies))]
+  return [
+    getCell(indicator.title),
+    ...studies.map((study) => getCell(indicator.getValue(study, studies), indicator.format))
+  ]
 }
 
 function getSubRows(studies, indicator) {
@@ -59,7 +115,16 @@ function getSubRows(studies, indicator) {
 
   const subKeys = getAllSubKeys(studies, indicator.getSubValues)
   return subKeys.map((subKey) => [
-    `---- ${subKey}`,
-    ...studies.map((study) => indicator.getSubValues(study, studies)[subKey])
+    getCell(`---- ${subKey}`),
+    ...studies.map((study) =>
+      getCell(indicator.getSubValues(study, studies)[subKey], indicator.format)
+    )
   ])
+}
+
+function getCell(value, type = 'string') {
+  return {
+    value,
+    type
+  }
 }
