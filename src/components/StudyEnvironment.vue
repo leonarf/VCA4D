d10e2c92d29ede06b453f50a517b4d153f809452
Maxime Piard <maxime@lebasic.com>
2024-11-13 11:51:51 +0100

    feat: Add specific rows for Global warming on comparison page (#140)
    Tache Notion (spec) :
    https://www.notion.so/le-basic/Ajouter-Global-warming-page-de-comparaison-1346afc9ad64800aae07cd14b7c71f64
    
    ## Contexte
    
    On vaut ajouter deux rangs sous les indicateurs environnementaux en `pt`
    pour extraire le Global warming (en eq CO2)
    
    
    ![image](https://github.com/user-attachments/assets/d5220ca5-e534-42af-bf73-d8b438717562)
    
    
    ## Changements
    
    - Un peu de boyscout
    - Ajout des rows global warming (en refactorant un peu en meme temps:
    merge de 2 commits que j'avais fait à la base)
    - :warning: Fix de l'algo qui calcule les impacts. Puisqu'on loopait sur
    les value chains, on tombait sur la value chain `Total production`
    (volume: total, impact: 0), ce qui divisait par deux l'impact réel par
    unité fonctionelle
    - J'ai vérifié à la main les calculs, ça colle maintenant bien avec la
    page d'environnement (sur 2 études cashew)

--- src/components/StudyEnvironment.vue
+++ src/components/StudyEnvironment.vue
@@ -3,34 +3,39 @@
     <SectionTitle>
       <h1>Is the value chain environmentally sustainable?</h1>
     </SectionTitle>
+    <p>The approach to evaluate the environmental sustainability of the value chain is twofold.</p>
     <p>
-      The approach to evaluate the environmental sustainability of the value chain is twofold.
+      First, damages entailed by the value chain operations are calculated on
+      <strong>Resource depletion</strong>, <strong>Ecosystem quality</strong> and
+      <strong>Human health</strong>, as well as their contribution to
+      <strong>Climate change</strong> through the quantitative
+      <strong>Life Cycle Assessment (LCA)</strong>.
     </p>
     <p>
-      First, damages entailed by the value chain operations are calculated on <strong>Resource depletion</strong>,
-      <strong>Ecosystem quality</strong> and <strong>Human health</strong>, as well as their contribution
-      to <strong>Climate change</strong> through the quantitative <strong>Life Cycle Assessment (LCA)</strong>.
-    </p>
-    <p> 
-      Second, an <strong>exploratory assessment of biodiversity risks is provided</strong>.
-      The appraisal of the environmental sustainability of the value chain is carried out by combining quantitative and qualitative data.
+      Second, an <strong>exploratory assessment of biodiversity risks is provided</strong>. The
+      appraisal of the environmental sustainability of the value chain is carried out by combining
+      quantitative and qualitative data.
     </p>
     <p class="mt-4">
-      LCA inventories the material and energy flows used, produced or released by the activities of the value chain.
-      The substances emitted or consumed by the activities at each stage are recorded and measured.
-      According to their physical, chemical and biological nature, they activate cause-and-effect
-      chains that induce changes in the environment. These changes cause (or counteract) specific
-      environmental problems such as terrestrial acidification, freshwater deprivation or ecotoxicity.
-    </p><p>
-      LCA refers to the effects as <strong>“impacts”</strong> (the “midpoints” level). The consequences of these impacts 
-      on Natural Resources, Ecosystem Quality and Human Health are referred to as <strong>“damage”</strong>. 
-      LCA also enables to measure the contribution of the value chain to climate change through its <strong>carbon footprint</strong>.
+      LCA inventories the material and energy flows used, produced or released by the activities of
+      the value chain. The substances emitted or consumed by the activities at each stage are
+      recorded and measured. According to their physical, chemical and biological nature, they
+      activate cause-and-effect chains that induce changes in the environment. These changes cause
+      (or counteract) specific environmental problems such as terrestrial acidification, freshwater
+      deprivation or ecotoxicity.
+    </p>
+    <p>
+      LCA refers to the effects as <strong>“impacts”</strong> (the “midpoints” level). The
+      consequences of these impacts on Natural Resources, Ecosystem Quality and Human Health are
+      referred to as <strong>“damage”</strong>. LCA also enables to measure the contribution of the
+      value chain to climate change through its <strong>carbon footprint</strong>.
     </p>
 
     <QuestionTitle>Which sub-chain contributes the most to environmental damages?</QuestionTitle>
     <p>
-      Sub-chains can be compared according to the damage they generate in the three areas of protection. 
-      This highlights the gaps between them and helps determine actions for environmental improvements.
+      Sub-chains can be compared according to the damage they generate in the three areas of
+      protection. This highlights the gaps between them and helps determine actions for
+      environmental improvements.
     </p>
     <div class="unit-selection">
       <div class="unit-selection-title">Unit Selection</div>
@@ -39,13 +44,13 @@
           title="Scope of the results"
           :options="perUnits"
           :selected="selectedPerUnit"
-          @update:selected="$event => selectedPerUnit = $event"
+          @update:selected="($event) => (selectedPerUnit = $event)"
         />
         <RadioInput
           title="Unit of the impact"
           :options="units"
           :selected="selectedUnit"
-          @update:selected="$event => selectedUnit = $event"
+          @update:selected="($event) => (selectedUnit = $event)"
         />
       </div>
     </div>
@@ -62,7 +67,7 @@ import { computed, ref } from 'vue'
 import SectionTitle from '@typography/SectionTitle.vue'
 import ImpactDataviz from '@components/study/environment/ImpactDataviz.vue'
 import { ACVImpacts } from '@utils/misc.js'
-import QuestionTitle from "@components/study/QuestionTitle.vue"
+import QuestionTitle from '@components/study/QuestionTitle.vue'
 import RadioInput from '@components/study/RadioInput.vue'
 
 const props = defineProps({
@@ -75,43 +80,59 @@ const allBarChartsData = computed(() => {
     if (!majorImpactsNames.includes(impact.name)) {
       return false
     }
-    return selectedUnit.value === "PT" ? (impact.unit === 'Pt') : (impact.unit !== 'Pt')
-    })
+    return selectedUnit.value === 'PT' ? impact.unit === 'Pt' : impact.unit !== 'Pt'
+  })
 
   return impactsToDrawOnGraph
 })
 
 const yearlyVolumes = computed(() => {
   var result = {}
-  props.studyData.acvData.valuechains.map(item => {
+  props.studyData.acvData.valuechains.map((item) => {
     result[item.name] = item.volume
   })
   return result
 })
 
 const units = [
-  { label: 'Different units', value: 'OTHER', subtitle: 'Each impact is expressed in its own unit' },
-  { label: 'Single score (Pt)', value: 'PT', subtitle: 'Different impacts (climate, health, water etc.) are expressed in a single weighted unit.' },
-];
-
-const perUnits = [
-  { label: "Impact per functional unit", value: "functional unit", subtitle: "Compare best performing production systems" },
-  { label: "Total impact per year", value: "year", subtitle: "See which sub-chain currently has most impact in the country" },
+  {
+    label: 'Different units',
+    value: 'OTHER',
+    subtitle: 'Each impact is expressed in its own unit'
+  },
+  {
+    label: 'Single score (Pt)',
+    value: 'PT',
+    subtitle:
+      'Different impacts (climate, health, water etc.) are expressed in a single weighted unit.'
+  }
 ]
 
-const selectedUnit = ref(units[0].value);
-const selectedPerUnit = ref(perUnits[0].value);
+const perUnits = [
+  {
+    label: 'Impact per functional unit',
+    value: 'functional unit',
+    subtitle: 'Compare best performing production systems'
+  },
+  {
+    label: 'Total impact per year',
+    value: 'year',
+    subtitle: 'See which sub-chain currently has most impact in the country'
+  }
+]
 
+const selectedUnit = ref(units[0].value)
+const selectedPerUnit = ref(perUnits[0].value)
 </script>
 
 <style scoped lang="scss">
 .unit-selection {
   margin: 32px 0;
-  
+
   .unit-selection-title {
     text-transform: uppercase;
     font-weight: 700;
-    color: #8A8A8A;
+    color: #8a8a8a;
     margin-bottom: 16px;
   }
 
--- src/components/comparison/comparisonConfig/environment.js
+++ src/components/comparison/comparisonConfig/environment.js
@@ -5,35 +5,67 @@ export const environmentConfig = [
   {
     title: 'Total impact per year',
     subtitle: 'Value chain total impact (in pt)',
-    getValue: (study, studies) => getTotalImpacts(study, studies, true),
-    getSubValues: (study, studies) => getValuesByImpact(study, studies, true),
+    getValue: (study, studies) => {
+      const impactNames = getPtImpactsNames(studies)
+      return getTotalImpacts(study, impactNames, true)
+    },
+    getSubValues: (study, studies) => {
+      const impactNames = getPtImpactsNames(studies)
+      return getPtValuesByImpact(study, impactNames, true)
+    },
     format: 'number'
   },
   {
     title: 'Impact per functional unit',
     subtitle: 'Value chain total impact / volume',
-    getValue: (study, studies) => getTotalImpacts(study, studies, false),
-    getSubValues: (study, studies) => getValuesByImpact(study, studies, false),
+    getValue: (study, studies) => {
+      const impactNames = getPtImpactsNames(studies)
+      return getTotalImpacts(study, impactNames, false)
+    },
+    getSubValues: (study, studies) => {
+      const impactNames = getPtImpactsNames(studies)
+      return getPtValuesByImpact(study, impactNames, false)
+    },
+    format: 'number'
+  },
+  {
+    title: 'Total impact per year on Global warming',
+    subtitle: 'Value chain total impact in kg eqCO2',
+    getValue: (study) =>
+      getImpactValue('Global warming', study, {
+        singleScore: false,
+        sumTotalPerYear: true
+      }),
+    format: 'number'
+  },
+  {
+    title: 'Impact per functional unit on Global warming',
+    subtitle: 'Value chain total impact / volume',
+    getValue: (study) =>
+      getImpactValue('Global warming', study, {
+        singleScore: false,
+        sumTotalPerYear: false
+      }),
     format: 'number'
   }
 ]
 
-function getAvailableImpacts(studies) {
+function getAvailablePtImpacts(studies) {
   return studies
     .reduce((arr, study) => arr.concat(study.acvData?.impacts), [])
     .filter((item) => !!item)
     .filter((item) => item.unit === 'Pt')
 }
 
-function getImpacts(studies) {
-  const availableImpacts = getAvailableImpacts(studies)
+function getPtImpactsNames(studies) {
+  const availableImpacts = getAvailablePtImpacts(studies)
   return ACVImpacts.filter((item) =>
     availableImpacts.map((availableImpact) => availableImpact.name).includes(item.name)
-  )
+  ).map((impact) => impact.name)
 }
 
-function getTotalImpacts(study, studies, sumTotalPerYear = false) {
-  const valuesByImpact = getValuesByImpact(study, studies, sumTotalPerYear)
+function getTotalImpacts(study, impactNames, sumTotalPerYear = false) {
+  const valuesByImpact = getPtValuesByImpact(study, impactNames, sumTotalPerYear)
   if (_.isEmpty(valuesByImpact)) {
     return null
   }
@@ -41,41 +73,51 @@ function getTotalImpacts(study, studies, sumTotalPerYear = false) {
   return _.sumBy(Object.values(valuesByImpact))
 }
 
-function getValuesByImpact(study, studies, sumTotalPerYear = false) {
+function getPtValuesByImpact(study, impactNames, sumTotalPerYear = false) {
   const valuesByImpact = {}
-  const impacts = getImpacts(studies)
-  impacts.forEach((impact) => {
-    const impactValue = getImpactValue(impact, study, sumTotalPerYear)
+  impactNames.forEach((impactName) => {
+    const impactValue = getImpactValue(impactName, study, {
+      singleScore: true,
+      sumTotalPerYear
+    })
     if (_.isNull(impactValue)) {
       return
     }
-    var label = ACVImpacts.find((item) => item.name == impact.name).label
+    var label = ACVImpacts.find((item) => item.name == impactName).label
     if (label) {
       valuesByImpact[label] = impactValue
     } else {
-      valuesByImpact[impact.name] = impactValue
+      valuesByImpact[impactName] = impactValue
     }
   })
   return valuesByImpact
 }
 
-function getImpactValue(impact, study, sumTotalPerYear = false) {
+function getImpactValue(impactName, study, { singleScore, sumTotalPerYear = false }) {
   if (!study.acvData) {
     return null
   }
-  const valueChains = study.acvData.valuechains
   let totalPerYear = 0
   let totalVolume = 0
-  for (const { name, volume } of valueChains) {
-    const totalChainPerYear = study.acvData.impacts
-      .filter((i) => i.name === impact.name)
-      .filter((i) => i.unit === 'Pt')
-      .reduce((arr, item) => arr.concat(item.values), [])
-      .filter((val) => val.valuechain_name === name)
-      .map((val) => val.value * volume)
-      .reduce((s, item) => s + item, 0)
-    totalVolume += volume
-    totalPerYear += totalChainPerYear
-  }
+
+  const studyImpactsToSum = study.acvData.impacts
+    .filter((impact) => impact.name === impactName)
+    .filter((impact) => (singleScore ? impact.unit === 'Pt' : impact.unit !== 'Pt'))
+
+  studyImpactsToSum.forEach((impact) => {
+    const impactValuesByChainName = _.groupBy(impact.values, (value) => value.valuechain_name)
+
+    for (const valueChainName in impactValuesByChainName) {
+      const valueChainTotalImpact = _.sumBy(impactValuesByChainName[valueChainName], 'value')
+      const volume = findValueChainVolume(study.acvData.valuechains, valueChainName)
+      totalVolume += volume
+      totalPerYear += valueChainTotalImpact * volume
+    }
+  })
   return sumTotalPerYear ? totalPerYear : totalPerYear / totalVolume
+}
+
+function findValueChainVolume(valueChains, valueChainName) {
+  const valueChain = valueChains.find((chain) => chain.name === valueChainName)
+  return valueChain?.volume
 }
